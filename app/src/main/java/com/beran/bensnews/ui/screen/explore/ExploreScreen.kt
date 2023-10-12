003e1c5a43c33c4ed19401d5950c3fc86541244e
beranju <beranjusihombing1@gmail.com>
2023-10-12 19:17:28 +0700

    update ExploreAppBar view

--- app/src/main/java/com/beran/bensnews/ui/screen/explore/ExploreScreen.kt
+++ app/src/main/java/com/beran/bensnews/ui/screen/explore/ExploreScreen.kt
@@ -1,38 +1,23 @@
 package com.beran.bensnews.ui.screen.explore
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.beran.bensnews.R
 import com.beran.bensnews.ui.component.ErrorView
+import com.beran.bensnews.ui.screen.explore.component.ExploreAppBar
 import com.beran.bensnews.ui.screen.explore.component.ExploreTabSection
 import com.beran.bensnews.utils.Category
 import com.beran.core.domain.model.NewsModel
@@ -60,8 +45,6 @@ fun ExploreScreen(
         mutableStateOf("")
     }
     ExploreContent(
-        searchText = searchText,
-        onChangeSearchText = { searchText = it },
         navigateToSearch = navigateToSearch,
         news = state.news,
         isLoading = state.isLoading,
@@ -74,8 +57,6 @@ fun ExploreScreen(
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun ExploreContent(
-    searchText: String,
-    onChangeSearchText: (String) -> Unit,
     navigateToSearch: () -> Unit,
     news: List<NewsModel>,
     isLoading: Boolean,
@@ -88,25 +69,7 @@ private fun ExploreContent(
             .fillMaxSize()
             .padding(vertical = 16.dp, horizontal = 24.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(55.dp)
-                .background(MaterialTheme.colorScheme.surface)
-                .border(
-                    1.dp,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    shape = RoundedCornerShape(30.dp)
-                )
-                .clip(RoundedCornerShape(30.dp))
-                .padding(vertical = 10.dp, horizontal = 12.dp)
-                .clickable { navigateToSearch() }
-        ) {
-            Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(text = stringResource(R.string.search_hint))
-        }
+        ExploreAppBar(navigateToSearch = navigateToSearch)
         Spacer(modifier = Modifier.height(24.dp))
         if (error != null) {
             ErrorView(message = error)
@@ -128,13 +91,4 @@ fun getCategoryByIndex(index: Int): String {
         values[0].value
     }
 }
-
-
-//@Preview(showBackground = true)
-//@Composable
-//private fun ExploreContentPrev() {
-//    BensNewsTheme {
-////        ExploreContent(searchText = "", onChangeSearchText = {})
-//    }
-//}
 
--- app/src/main/java/com/beran/bensnews/ui/screen/explore/component/ExploreAppBar.kt
+++ app/src/main/java/com/beran/bensnews/ui/screen/explore/component/ExploreAppBar.kt
@@ -1,57 +1,53 @@
 package com.beran.bensnews.ui.screen.explore.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.beran.bensnews.R
 import com.beran.bensnews.ui.theme.BensNewsTheme
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExploreAppBar(
-    searchText: String,
-    onChangeSearchText: (String) -> Unit,
+    navigateToSearch: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        OutlinedTextField(
-            value = searchText,
-            onValueChange = onChangeSearchText,
-            placeholder = { Text(text = stringResource(R.string.search_hint)) },
-            leadingIcon = {
-                Icon(imageVector = Icons.Rounded.Search, contentDescription = "Search Icon")
-            },
-            trailingIcon = {
-                if (searchText.isNotBlank()) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = "Cancel Icon",
-                        modifier = Modifier.clickable { onChangeSearchText("") })
-                }
-            },
-            singleLine = true,
-            shape = RoundedCornerShape(30.dp),
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .background(MaterialTheme.colorScheme.surface)
+            .border(
+                1.dp,
+                color = MaterialTheme.colorScheme.onSurface,
+                shape = RoundedCornerShape(30.dp)
+            )
+            .clip(RoundedCornerShape(30.dp))
+            .padding(vertical = 10.dp, horizontal = 12.dp)
+            .clickable { navigateToSearch() }
+    ) {
+        Icon(imageVector = Icons.Rounded.Search, contentDescription = null)
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = stringResource(R.string.search_hint))
     }
 }
 
@@ -59,6 +55,6 @@ fun ExploreAppBar(
 @Composable
 private fun ExploreAppBarPrev() {
     BensNewsTheme {
-        ExploreAppBar(searchText = "", onChangeSearchText = {})
+        ExploreAppBar(navigateToSearch = {})
     }
 }

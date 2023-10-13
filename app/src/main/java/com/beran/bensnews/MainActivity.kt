58e853b38ebec668fd93ec19125d6a13d389f809
beranju <beranjusihombing1@gmail.com>
2023-10-13 15:56:29 +0700

    implement splash screen api

--- app/src/main/java/com/beran/bensnews/MainActivity.kt
+++ app/src/main/java/com/beran/bensnews/MainActivity.kt
@@ -3,12 +3,14 @@ package com.beran.bensnews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.beran.bensnews.ui.theme.BensNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             BensNewsTheme {
                 val navController = rememberNavController()
